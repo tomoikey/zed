7796032dade7093d3cf47201df5681ca35a2bef5
Conrad Irwin <conrad.irwin@gmail.com>
2024-06-10 20:03:21 -0600

    Wait for composition to end before sending InputIgnored (#12871)
    Release Notes:
    
    - vim: Fixed `f`/`t` etc. for keys that require IME (#12522)

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -11821,7 +11821,6 @@ impl ViewInputHandler for Editor {
         cx: &mut ViewContext<Self>,
     ) {
         if !self.input_enabled {
-            cx.emit(EditorEvent::InputIgnored { text: text.into() });
             return;
         }
 
